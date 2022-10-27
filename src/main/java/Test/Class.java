0dff01173e77077cc3b1734b8d71d9eae5ce6222
rayan <rayanzyad99@gmail.com>
2022-10-27 11:08:25 +0100

    adding a add function

--- src/main/java/Test/Class.java
+++ src/main/java/Test/Class.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class Class {
     public static void main(String[] args) {
-add();
-    }
-    public static void add() {
-        Scanner myinput = new Scanner(System.in);
-        System.out.println("Please enter your number ");
-        int firstnumber = myinput.nextInt();
-        System.out.println("Please enter your second number ");
-        int secondnumber = myinput.nextInt();
 
-        int total = firstnumber + secondnumber;
-        System.out.println("The total is " + total);
+        add();
     }
-}
+            public static void add() {
+                Scanner myinput = new Scanner(System.in);
+                System.out.println("Please enter your number ");
+                int firstnumber = myinput.nextInt();
+                System.out.println("Please enter your second number ");
+                int secondnumber = myinput.nextInt();
+
+                int total = firstnumber + secondnumber;
+                System.out.println("The total is " + total);
+            }
+            }
+
